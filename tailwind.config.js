18f940c7419bd3eb983dcb12fd5f261cd0ab3dd8
Roger Rojas <roger.rojas.effio@gmail.com>
2020-04-11 13:07:55 -0500

    archivos y configuracion inicial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#31afb4',
+        secondary: '#115571',
+        tagline: '#14212d',
+        complementary: '#b43631'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
